59c1455efde493db2f595a7da1167ca20fd05571
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-07-24 09:39:53 +0200

    sauvegarde

---
+++ src/Sites/Admin/Common/StaticPageManagerBundle/Entity/StaticPage.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Sites\Admin\Common\StaticPageManagerBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * StaticPage
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Sites\Admin\Common\StaticPageManagerBundle\Entity\StaticPageRepository")
+ */
+class StaticPage
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return StaticPage
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return StaticPage
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+}
